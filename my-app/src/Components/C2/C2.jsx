5040102e6f81b20556a7eef3c437fc34d65018ff
Varun <jvkumar560@outlook.com>
2023-12-02 22:22:01 +0530

    S2

--- my-app/src/Components/C2/C2.jsx
+++ my-app/src/Components/C2/C2.jsx
@@ -2,13 +2,27 @@ import "./C2.css";
 
 import React from "react";
 
-const C2 = ({ s1, s2, s3, s4, s5, s6, s7, b1, b2, b3, img1, img2,flag }) => {
+const C2 = ({
+  s1,
+  s2,
+  s3,
+  s4,
+  s5,
+  s6,
+  s7,
+  b1,
+  b2,
+  b3,
+  img1,
+  img2,
+  color,
+  flag,
+}) => {
   return (
-    <div className="c2-container" style={{ flexDirection: "row" }}>
+    <div className={flag ? "c2-container" : "c2-container c2-container-right"}>
       <div className="c2-left">
-        <span className="tag">{s1}</span>
+              <span className="tag" style={{ backgroundColor: color }} >{s1}</span>
         <span>
-          {" "}
           {s2}
           <br />
           {s3}
@@ -18,12 +32,11 @@ const C2 = ({ s1, s2, s3, s4, s5, s6, s7, b1, b2, b3, img1, img2,flag }) => {
           <br /> {s6}
           <br /> {s7}
         </span>
-        <div className={flag?"left-bubble":"left-bubble right-bubble"}>
+        <div className={flag ? "left-bubble" : "left-bubble right-bubble"}>
           <img src={img1} alt="" />
           <div>
             <span>
               {b1}
-              <br /> {b2}
             </span>
             <span>{b3}</span>
           </div>
--- my-app/src/Components/Series/Series.jsx
+++ my-app/src/Components/Series/Series.jsx
@@ -14,13 +14,61 @@ const Series = () => {
         s6="import events from emails, and
           add participants with just "
         s7="a few clicks."
-        b1="I tested many calendar apps. This app is the "
-        b2="best of all!
+        b1="I tested many calendar apps. This app is the best of all!
               It saves me hours of time."
         b3="Carla, Head of Finance "
         img1="https://framerusercontent.com/images/Pm9TTOkN341IYtePLdPGmhG0E.png"
-              img2="https://framerusercontent.com/images/KsZ6wO13QZewZKHzx7JNgFn88.png"
-              flag={false}
+        img2="https://framerusercontent.com/images/KsZ6wO13QZewZKHzx7JNgFn88.png"
+        color="rgba(254, 233, 203, 1)"
+        flag={true}
+      />
+      <C2
+        s1="Smart Reminders & Task"
+        s2="Never miss an important "
+        s3="deadline or event again"
+        s4="Never miss an important deadline again with our AI-driven "
+        s5="notifications. Our app intelligently analyzes your schedule "
+        s6="to prioritize what's most important, keeping you on track"
+        s7="and ensuring your day is productive and stress-free."
+        b1="I tested many calendar apps. This app is the easy to add events and set reminders!"
+        b3="Adam, entrepreneur"
+        img1="https://framerusercontent.com/images/CPGgYEBeFy4gDXe5dDzh1qjQG1w.png"
+        img2="https://framerusercontent.com/images/EgCwW6bpD4t2JQoyjgySRkUXc.png?scale-down-to=1024"
+        color="rgba(204, 234, 253, 1)"
+        flag={false}
+      />
+      <C2
+        s1="Seamless Scheduling"
+        s2="Focus on what matters most"
+        s3="for you"
+        s4="Effortlessly plan your day with our intuitive drag-and-"
+        s5="drop interface.
+          Sync with multiple calendar platforms, "
+        s6="import events from emails, and
+          add participants with just "
+        s7="a few clicks."
+        b1="I've tried a lot of calendar apps, but this one is by far the best! It's so intuitive and customizable, and it has all the features I need."
+        b3="Annie, Designer"
+        img1="https://framerusercontent.com/images/1OwzdZHx9ElDw1ZAavs6WGWBSw.png"
+        img2="https://framerusercontent.com/images/xjG69OjCFt0Z3YQvNa3HNBBP5G0.png?scale-down-to=1024"
+        color="#DDF4E4"
+        flag={true}
+      />
+
+      <C2
+        s1="Smart Reminders & Task"
+        s2="Never miss an important "
+        s3="deadline or event again"
+        s4="Never miss an important deadline again with our AI-driven "
+        s5="notifications. Our app intelligently analyzes your schedule "
+        s6="to prioritize what's most important, keeping you on track"
+        s7="and ensuring your day is productive and stress-free."
+        b1="I've recommended this app to all my friends and colleagues! "
+        b3="Karl, CTO of a cool startup"
+        img1="https://framerusercontent.com/images/lKaaStgbmdIe8TfrIkFlKzHazV0.png"
+        img2="https://framerusercontent.com/images/ow5cq9Ercs5KlWFqhMUsdUkCl8.png?scale-down-to=1024"
+        color="rgba(204, 234, 253, 1)"
+        flag={false}
       />
     </>
   );
